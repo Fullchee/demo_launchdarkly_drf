0440e049eec13efe999496bfabea7f8f32301379
Fullchee Zhang <fullchee@gmail.com>
2025-12-25 13:41:01 -0500

    [skip ci] Get a view

---
+++ mysite/mysite/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ["url", "username", "email", "is_staff"]
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+@api_view(["GET"])
+def test_view(request: Request) -> Response:
+    return Response("Test view is working!")
